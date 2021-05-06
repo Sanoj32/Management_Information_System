fc5d87f0bcbf0fbe3766619bca95c7e345c21894
sanoj23 <sanoj.shrestha.13@gmail.com>
2021-05-06 09:28:20 +0545

    testing on heroku

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,20 +18,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
-        // BctStudent::factory()->count(10)->create();
-        // Admin::firstorCreate([
-        //     'name' => "Sanoj Raj Shrestha",
-        //     'email' => "sanoj.shrestha.13@gmail.com",
-        //     'password' => Hash::make('adminpass'),
-        // ]);
-        $jsondata = json_decode(file_get_contents(storage_path('/json_data/bct_subjects.json')), true);
-        $storedSubjects = BctSubject::all(['subject_code'])->toArray();
-        foreach ($jsondata as $data) {
-            // if (in_array($data->subject_code, $storedSubjects)) {
-            //     echo "Its here";
-            // }
-            echo $data->name;
-        }
+        User::factory(10)->create();
+        BctStudent::factory()->count(10)->create();
+        Admin::firstorCreate([
+            'name' => "Sanoj Raj Shrestha",
+            'email' => "sanoj.shrestha.13@gmail.com",
+            'password' => Hash::make('adminpass'),
+        ]);
+        // $jsondata = json_decode(file_get_contents(storage_path('/json_data/bct_subjects.json')), true);
+        // $storedSubjects = BctSubject::all(['subject_code'])->toArray();
+        // foreach ($jsondata as $data) {
+        //     // if (in_array($data->subject_code, $storedSubjects)) {
+        //     //     echo "Its here";
+        //     // }
+        //     echo $data->name;
+        // }
     }
 }
