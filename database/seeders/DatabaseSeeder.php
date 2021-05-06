6695db5f67c8e10b09432325c23e51197d04ca7d
sanoj23 <sanoj.shrestha.13@gmail.com>
2021-05-06 18:06:49 +0545

    Subject seeder created Subject seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,13 +25,24 @@ class DatabaseSeeder extends Seeder
             'email' => "sanoj.shrestha.13@gmail.com",
             'password' => Hash::make('adminpass'),
         ]);
-        // $jsondata = json_decode(file_get_contents(storage_path('/json_data/bct_subjects.json')), true);
-        // $storedSubjects = BctSubject::all(['subject_code'])->toArray();
-        // foreach ($jsondata as $data) {
-        //     // if (in_array($data->subject_code, $storedSubjects)) {
-        //     //     echo "Its here";
-        //     // }
-        //     echo $data->name;
-        // }
+        $jsondata = json_decode(file_get_contents(storage_path('/json_data/bct_subjects.json')), true);
+        $storedSubjects = BctSubject::all(['subject_code']);
+        $storedSubArray = array();
+        foreach ($storedSubjects as $sub) {
+            array_push($storedSubArray, $sub['subject_code']);
+        }
+        foreach ($jsondata as $data) {
+            if (in_array($data['subject_code'], $storedSubArray)) {
+                echo "DUPLICATE ENTRY!!!!!!!!!!!!!!!";
+                continue;
+            }
+            $subject = new BctSubject();
+            $subject['name'] = $data['name'];
+            $subject['subject_code'] = $data['subject_code'];
+            $subject['semester'] = $data['semester'];
+            $subject->save();
+            echo ('NEW SUBJECT ENTRY CREATED!!!!!!!!!!!!!!!');
+            array_push($storedSubArray, $data['subject_code']);
+        }
     }
 }
